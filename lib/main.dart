690101bef98775fdd304f675b233397c6d1d78b5
hasibarrafiul <fahad.com66.fk@gmail.com>
2022-11-22 20:07:47 +0600

    reset button added

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -25,6 +27,7 @@ class _MyAppState extends State<MyApp> {
   String button9 = '';
   String turn = 'p1';
   String winner = '';
+  bool clickable = true;
 
   @override
   void initState() {
@@ -41,51 +44,67 @@ class _MyAppState extends State<MyApp> {
   void checkWinner() {
     if (button1 == "X" && button2 == "X" && button3 == "X") {
       winner = "X";
+      clickable = false;
     }
     if (button4 == "X" && button5 == "X" && button6 == "X") {
       winner = "X";
+      clickable = false;
     }
     if (button7 == "X" && button8 == "X" && button9 == "X") {
       winner = "X";
+      clickable = false;
     }
     if (button1 == "X" && button4 == "X" && button7 == "X") {
       winner = "X";
+      clickable = false;
     }
     if (button2 == "X" && button5 == "X" && button8 == "X") {
       winner = "X";
+      clickable = false;
     }
     if (button3 == "X" && button6 == "X" && button9 == "X") {
       winner = "X";
+      clickable = false;
     }
     if (button1 == "X" && button5 == "X" && button9 == "X") {
       winner = "X";
+      clickable = false;
     }
     if (button3 == "X" && button5 == "X" && button7 == "X") {
       winner = "X";
+      clickable = false;
     }
     if (button1 == "O" && button2 == "O" && button3 == "O") {
       winner = "O";
+      clickable = false;
     }
     if (button4 == "O" && button5 == "O" && button6 == "O") {
       winner = "O";
+      clickable = false;
     }
     if (button7 == "O" && button8 == "O" && button9 == "O") {
       winner = "O";
+      clickable = false;
     }
     if (button1 == "O" && button4 == "O" && button7 == "O") {
       winner = "O";
+      clickable = false;
     }
     if (button2 == "O" && button5 == "O" && button8 == "O") {
       winner = "O";
+      clickable = false;
     }
     if (button3 == "O" && button6 == "O" && button9 == "O") {
       winner = "O";
+      clickable = false;
     }
     if (button1 == "O" && button5 == "O" && button9 == "O") {
       winner = "O";
+      clickable = false;
     }
     if (button3 == "O" && button5 == "O" && button7 == "O") {
       winner = "O";
+      clickable = false;
     }
 
     if (button1 != "" &&
@@ -98,6 +117,7 @@ class _MyAppState extends State<MyApp> {
         button8 != "" &&
         button9 != "") {
       winner = "Draw";
+      clickable = false;
     }
   }
 
@@ -117,15 +137,18 @@ class _MyAppState extends State<MyApp> {
                   child: new Text('$button1'),
                   onPressed: () {
                     setState(() {
-                      if (button1 == '') {
-                        if (turn == 'p1') {
-                          button1 = "X";
-                          turn = 'p2';
-                        } else if (turn == 'p2') {
-                          button1 = "O";
-                          turn = 'p1';
+                      if (clickable == true) {
+                        if (button1 == '') {
+                          if (turn == 'p1') {
+                            button1 = "X";
+                            turn = 'p2';
+                          } else if (turn == 'p2') {
+                            button1 = "O";
+                            turn = 'p1';
+                          }
                         }
                       }
+
                       ;
                     });
                     checkWinner();
@@ -135,15 +158,18 @@ class _MyAppState extends State<MyApp> {
                   child: new Text('$button2'),
                   onPressed: () {
                     setState(() {
-                      if (button2 == '') {
-                        if (turn == 'p1') {
-                          button2 = "X";
-                          turn = 'p2';
-                        } else if (turn == 'p2') {
-                          button2 = "O";
-                          turn = 'p1';
+                      if (clickable == true) {
+                        if (button2 == '') {
+                          if (turn == 'p1') {
+                            button2 = "X";
+                            turn = 'p2';
+                          } else if (turn == 'p2') {
+                            button2 = "O";
+                            turn = 'p1';
+                          }
                         }
                       }
+
                       ;
                     });
                     checkWinner();
@@ -153,13 +179,15 @@ class _MyAppState extends State<MyApp> {
                   child: new Text('$button3'),
                   onPressed: () {
                     setState(() {
-                      if (button3 == '') {
-                        if (turn == 'p1') {
-                          button3 = "X";
-                          turn = 'p2';
-                        } else if (turn == 'p2') {
-                          button3 = "O";
-                          turn = 'p1';
+                      if (clickable == true) {
+                        if (button3 == '') {
+                          if (turn == 'p1') {
+                            button3 = "X";
+                            turn = 'p2';
+                          } else if (turn == 'p2') {
+                            button3 = "O";
+                            turn = 'p1';
+                          }
                         }
                       }
                       ;
@@ -177,15 +205,18 @@ class _MyAppState extends State<MyApp> {
                   child: new Text('$button4'),
                   onPressed: () {
                     setState(() {
-                      if (button4 == '') {
-                        if (turn == 'p1') {
-                          button4 = "X";
-                          turn = 'p2';
-                        } else if (turn == 'p2') {
-                          button4 = "O";
-                          turn = 'p1';
+                      if (clickable == true) {
+                        if (button4 == '') {
+                          if (turn == 'p1') {
+                            button4 = "X";
+                            turn = 'p2';
+                          } else if (turn == 'p2') {
+                            button4 = "O";
+                            turn = 'p1';
+                          }
                         }
                       }
+
                       ;
                     });
                     checkWinner();
@@ -195,13 +226,15 @@ class _MyAppState extends State<MyApp> {
                   child: new Text('$button5'),
                   onPressed: () {
                     setState(() {
-                      if (button5 == '') {
-                        if (turn == 'p1') {
-                          button5 = "X";
-                          turn = 'p2';
-                        } else if (turn == 'p2') {
-                          button5 = "O";
-                          turn = 'p1';
+                      if (clickable == true) {
+                        if (button5 == '') {
+                          if (turn == 'p1') {
+                            button5 = "X";
+                            turn = 'p2';
+                          } else if (turn == 'p2') {
+                            button5 = "O";
+                            turn = 'p1';
+                          }
                         }
                       }
                       ;
@@ -213,13 +246,15 @@ class _MyAppState extends State<MyApp> {
                   child: new Text('$button6'),
                   onPressed: () {
                     setState(() {
-                      if (button6 == '') {
-                        if (turn == 'p1') {
-                          button6 = "X";
-                          turn = 'p2';
-                        } else if (turn == 'p2') {
-                          button6 = "O";
-                          turn = 'p1';
+                      if (clickable == true) {
+                        if (button6 == '') {
+                          if (turn == 'p1') {
+                            button6 = "X";
+                            turn = 'p2';
+                          } else if (turn == 'p2') {
+                            button6 = "O";
+                            turn = 'p1';
+                          }
                         }
                       }
                       ;
@@ -237,15 +272,18 @@ class _MyAppState extends State<MyApp> {
                   child: new Text('$button7'),
                   onPressed: () {
                     setState(() {
-                      if (button7 == '') {
-                        if (turn == 'p1') {
-                          button7 = "X";
-                          turn = 'p2';
-                        } else if (turn == 'p2') {
-                          button7 = "O";
-                          turn = 'p1';
+                      if (clickable == true) {
+                        if (button7 == '') {
+                          if (turn == 'p1') {
+                            button7 = "X";
+                            turn = 'p2';
+                          } else if (turn == 'p2') {
+                            button7 = "O";
+                            turn = 'p1';
+                          }
                         }
                       }
+
                       ;
                     });
                     checkWinner();
@@ -255,13 +293,15 @@ class _MyAppState extends State<MyApp> {
                   child: new Text('$button8'),
                   onPressed: () {
                     setState(() {
-                      if (button8 == '') {
-                        if (turn == 'p1') {
-                          button8 = "X";
-                          turn = 'p2';
-                        } else if (turn == 'p2') {
-                          button8 = "O";
-                          turn = 'p1';
+                      if (clickable == true) {
+                        if (button8 == '') {
+                          if (turn == 'p1') {
+                            button8 = "X";
+                            turn = 'p2';
+                          } else if (turn == 'p2') {
+                            button8 = "O";
+                            turn = 'p1';
+                          }
                         }
                       }
                       ;
@@ -273,13 +313,15 @@ class _MyAppState extends State<MyApp> {
                   child: new Text('$button9'),
                   onPressed: () {
                     setState(() {
-                      if (button9 == '') {
-                        if (turn == 'p1') {
-                          button9 = "X";
-                          turn = 'p2';
-                        } else if (turn == 'p2') {
-                          button9 = "O";
-                          turn = 'p1';
+                      if (clickable == true) {
+                        if (button9 == '') {
+                          if (turn == 'p1') {
+                            button9 = "X";
+                            turn = 'p2';
+                          } else if (turn == 'p2') {
+                            button9 = "O";
+                            turn = 'p1';
+                          }
                         }
                       }
                       ;
@@ -291,6 +333,25 @@ class _MyAppState extends State<MyApp> {
             ),
           ),
           Text('Winner is : $winner'),
+          ElevatedButton(
+            child: new Text("Reset"),
+            onPressed: () {
+              setState(() {
+                button1 = '';
+                button2 = '';
+                button3 = '';
+                button4 = '';
+                button5 = '';
+                button6 = '';
+                button7 = '';
+                button8 = '';
+                button9 = '';
+                turn = 'p1';
+                winner = '';
+                clickable = true;
+              });
+            },
+          ),
         ],
       ),
     );
